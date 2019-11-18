83de034929e56a915e94e6764743fc232ae6832c
Kitsunetic <kitsune.guru@gmail.com>
2019-11-18 20:34:55 +0900

    Create list.go

---
+++ list.go
@@ -0,0 +1,68 @@
+package recursivelist
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+/*
+ListRecurrent Lists files of a directory recurrently using channels.
+The output paths will be appeared with relative path.
+
+directory string - the directory path to list files
+*/
+func ListRecurrent(directory string) (chan string, chan error, chan bool) {
+	return listRecurrent(directory, "/")
+}
+
+func listRecurrent(directory, root string) (chan string, chan error, chan bool) {
+	out := make(chan string)
+	errs := make(chan error)
+	done := make(chan bool)
+
+	dir, err := filepath.Abs(directory)
+	if err != nil {
+		errs <- err
+	}
+
+	matches, err := filepath.Glob(filepath.Join(dir, "*"))
+	if err != nil {
+		errs <- err
+	}
+
+	go func() {
+		for _, file := range matches {
+			_, fname := filepath.Split(file)
+			if strings.HasPrefix(fname, ".") {
+				continue
+			}
+
+			stat, err := os.Stat(file)
+			if err != nil {
+				errs <- err
+			}
+
+			switch mode := stat.Mode(); {
+			case mode.IsRegular():
+				out <- filepath.Join(root, fname)
+			case mode.IsDir():
+				ifiles, ierrs, idone := listRecurrent(file, filepath.Join(root, fname))
+			L:
+				for {
+					select {
+					case ifile := <-ifiles:
+						out <- ifile
+					case ierr := <-ierrs:
+						errs <- ierr
+					case <-idone:
+						break L
+					}
+				}
+			}
+		}
+		done <- true
+	}()
+
+	return out, errs, done
+}
